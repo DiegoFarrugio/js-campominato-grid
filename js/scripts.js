a5d64338aa7849c79f5fe7f05288b3609ce6e6d6
DiegoFarrugio <diegofarrugio0@gmail.com>
2023-12-13 18:05:53 +0100

    Aggiungo commento

--- js/scripts.js
+++ js/scripts.js
@@ -12,23 +12,25 @@ Le validazioni e i controlli possiamo farli anche in un secondo momento.*/
 
 
 
-const startGame = alert('Clicca OK qui per avviare il tuo gioco!')
+const avvioGrigliaGioco = alert('Clicca OK per avviare il gioco!')
+
+const containerGrid = document.querySelector ('.container-grid')  //Mi seleziono il container grid dell'Html
+
+/*const playButton = document.getElementById("play_button");
+console.log('playButton', playButton, typeof playButton);*/
 
 
-const containerGrid = document.querySelector ('.container-grid')
+for(let i = 1; i <= 100; i++){ 
+    const cell = document.createElement ('div'); //Così facendo creo i DIV e al momento mi fa vedere solo i numeri in pagina
+    cell.classList.add('cell'); //Aggiungo la classe cell richiamandola dal CSS così da dare lo stile
+    cell.innerHTML = i; //Aggiungo questo per scrivere il numero all'interno di ogni cella
+
+    containerGrid.append(cell); //Dopo aver fatto tutto inserisco le celle dentro il containterGrid con l'append
 
 
-for(let i = 1; i <= 100; i++){
-    const cell = document.createElement ('div');
-    cell.classList.add('cell');
-    cell.innerHTML = i;
-    containerGrid.append(cell);
-
-    cell.addEventListener('click', function(){
-        cell.classList.toggle('click-active');
+    cell.addEventListener('click', function(){ //Creo un evento che al click della CELLA mi generi: 
+        cell.classList.toggle('click-active'); //Toggle serve per selezionare e deselezionare col click un elemento. Con classlist e poi'click-active' richiamo il CSS per dare stile.
         console.log(this); 
-        cell.classList.toggle ('active');
-        console.log(cell.innerHTML); 
-    })
-    
+        console.log(cell.innerHTML); //Con questo spariamo in console tutti i numeri che l'utente seleziona
+    })  
 }
