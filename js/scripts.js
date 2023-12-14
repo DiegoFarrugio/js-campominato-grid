a7b10a0a6c3c5064448d6b79f6dc18f5de3f4d74
DiegoFarrugio <diegofarrugio0@gmail.com>
2023-12-14 01:19:19 +0100

    Aggiungo bottone

--- js/scripts.js
+++ js/scripts.js
@@ -11,26 +11,52 @@ Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dat
 Le validazioni e i controlli possiamo farli anche in un secondo momento.*/
 
 
+const myButton = document.getElementById('myButton');
 
-const avvioGrigliaGioco = alert('Clicca OK per avviare il gioco!')
+myButton.addEventListener('click', function(){
 
-const containerGrid = document.querySelector ('.container-grid')  //Mi seleziono il container grid dell'Html
+    const containerGrid = document.querySelector ('.container-grid')  //Mi seleziono il container grid dell'Html
 
-/*const playButton = document.getElementById("play_button");
-console.log('playButton', playButton, typeof playButton);*/
+    for(let i = 1; i <= 100; i++){ 
+        const cell = document.createElement ('div'); //Così facendo creo i DIV e al momento mi fa vedere solo i numeri in pagina
+        cell.classList.add('cell'); //Aggiungo la classe cell richiamandola dal CSS così da dare lo stile
+        cell.innerHTML = i; //Aggiungo questo per scrivere il numero all'interno di ogni cella
+    
+        containerGrid.append(cell); //Dopo aver fatto tutto inserisco le celle dentro il containterGrid con l'append
+    
+    
+        cell.addEventListener('click', function(){ //Creo un evento che al click della CELLA mi generi: 
+            cell.classList.toggle('click-active'); //Toggle serve per selezionare e deselezionare col click un elemento. Con classlist e poi'click-active' richiamo il CSS per dare stile.
+            console.log(this); 
+            console.log(cell.innerHTML); //Con questo spariamo in console tutti i numeri che l'utente seleziona
+        })  
+    } 
+
+})
 
 
-for(let i = 1; i <= 100; i++){ 
-    const cell = document.createElement ('div'); //Così facendo creo i DIV e al momento mi fa vedere solo i numeri in pagina
-    cell.classList.add('cell'); //Aggiungo la classe cell richiamandola dal CSS così da dare lo stile
-    cell.innerHTML = i; //Aggiungo questo per scrivere il numero all'interno di ogni cella
-
-    containerGrid.append(cell); //Dopo aver fatto tutto inserisco le celle dentro il containterGrid con l'append
 
 
-    cell.addEventListener('click', function(){ //Creo un evento che al click della CELLA mi generi: 
-        cell.classList.toggle('click-active'); //Toggle serve per selezionare e deselezionare col click un elemento. Con classlist e poi'click-active' richiamo il CSS per dare stile.
-        console.log(this); 
-        console.log(cell.innerHTML); //Con questo spariamo in console tutti i numeri che l'utente seleziona
-    })  
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
